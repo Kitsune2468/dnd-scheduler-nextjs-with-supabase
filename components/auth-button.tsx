2f37ea7d73fe06d9d38d19616ae4d7088530260e
Kitsune2468 <masefro@gmail.com>
2025-06-16 19:02:29 -0600

    Checking auth button error

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -6,38 +6,17 @@ import { LogoutButton } from "./logout-button";
 export async function AuthButton() {
   const supabase = await createClient();
   
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if(user) {
-    const {
-    data: profile
-    } = await supabase.from('profiles')
-                      .select('*')
-                      .eq('id', user.id)
-                      .single();
-
-    if(profile.display_name) {
-      return (
-        <div className="flex items-center gap-4">
-          {profile.display_name}
-          <LogoutButton />
-        </div>
-      );
-    } else {
-      return (
-        <div className="flex items-center gap-4">
-          {user.email}
-          <LogoutButton />
-        </div>
-      );
-    }
-    
-  } else {
-    return (
-      <div className="flex gap-2">
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hey, {user.email}!
+      <LogoutButton />
+    </div>
+  ) : (
+    <div className="flex gap-2">
         <Button asChild size="sm" variant={"outline"} className="text-black">
           <Link href="/auth/login">Sign in</Link>
         </Button>
@@ -45,8 +24,5 @@ export async function AuthButton() {
           <Link href="/auth/sign-up">Sign up</Link>
         </Button>
       </div>
-    );
-  }
-
+  );
 }
-
